22100d16889d9b1adddb98b125fbef6b9394e615
michael <91007280+michaelHookon@users.noreply.github.com>
2022-11-25 17:17:38 +0800

    Update RBD.sol

--- Solidity-contract/token20/RBD.sol
+++ Solidity-contract/token20/RBD.sol
@@ -5,15 +5,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract RBD is ERC20,Ownable{
     address public admin;
     event AdminChange(address indexed Admin, address indexed newAdmin);
-    constructor(address manager) public ERC20("RBD", "RBD") {
-        admin = manager;
+    constructor() ERC20("RBD", "RBD") {
+        
         _mint(manager, 10_000_000_000 *10 ** 18);
     }
  
-    function changeOwner(address manager) external onlyOwner {
-        admin = manager;
-        emit AdminChange(msg.sender,manager);
-    }
     function burn(address account, uint256 amount) external onlyOwner{
         _burn(account, amount);
     }
