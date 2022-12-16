a2aa4091f54e86296f6d2d095fe8563af8569a6b
michael <91007280+michaelHookon@users.noreply.github.com>
2022-12-16 13:50:19 +0800

    Update RBD.sol

--- Solidity-contract/token20/RBD.sol
+++ Solidity-contract/token20/RBD.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 contract RBD is ERC20,Ownable{
-    address public admin;
     event AdminChange(address indexed Admin, address indexed newAdmin);
     constructor() ERC20("RBD", "RBD") {
         
