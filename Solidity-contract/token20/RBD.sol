7db6fdd60d2f202ab0ea68a68914bd2347c9cb2d
michael <91007280+michaelHookon@users.noreply.github.com>
2022-12-27 17:29:49 +0800

    Update RBD.sol

--- Solidity-contract/token20/RBD.sol
+++ Solidity-contract/token20/RBD.sol
@@ -3,13 +3,21 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 contract RBD is ERC20,Ownable{
+    address public mintAddr;
     event AdminChange(address indexed Admin, address indexed newAdmin);
     constructor() ERC20("RBD", "RBD") {
         
         _mint(msg.sender, 10_000_000_000 *10 ** 18);
     }
- 
-    function burn(address account, uint256 amount) external onlyOwner{
+    function setMintAddress(address _mintAddr) public onlyOwner{
+    mintAddr = _mintAddr;
+    }
+    function mint(address account, uint256 amount ) external {
+        require(msg.sender == mintAddr,'u are not mint address');
+        _mint(account, amount);
+    }
+    function burn(address account, uint256 amount) external {
+        require(msg.sender == mintAddr,'u are not burn address');
         _burn(account, amount);
     }
 }
